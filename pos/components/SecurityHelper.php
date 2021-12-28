2f280bbb7826240fce5ac807fd15538ea945b60d
donniwibowo <donniwibowo15@gmail.com>
2021-12-28 12:40:17 +0700

    Update SecurityHelper.php
    Change encryption method

--- pos/components/SecurityHelper.php
+++ pos/components/SecurityHelper.php
@@ -4,6 +4,11 @@
 	{
 		static $skey = "3917b28f82e9d0f630f48e89f2c91de7"; // you can change it
 
+		static $ciphering = "DC-DCJ";
+		static $iv_length = openssl_cipher_iv_length($ciphering);
+		static $options = 0;
+			
+
 		public static function safe_b64encode($string)
 		{
 			$data = base64_encode($string);
@@ -28,9 +33,13 @@
 			if($skey == '') {
 				$skey = self::$skey;
 			}
-            
-            return md5($input);
-        
+
+			$encryption_iv = random_bytes(self::$iv_length);
+			$encryption_key = openssl_digest(php_uname(), 'MD5', TRUE);
+
+			return openssl_encrypt($input, self::$ciphering, $encryption_key, self::$options, $encryption_iv);
+			
+            // return md5($input);
 			//return trim(self::url_base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $skey, $input, MCRYPT_MODE_ECB, mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB), MCRYPT_RAND))));
 
 		}
@@ -41,7 +50,12 @@
 				$skey = self::$skey;
 			}
 
-			return trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $skey, self::url_base64_decode($input), MCRYPT_MODE_ECB, mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB), MCRYPT_RAND)));
+			$encryption_iv = random_bytes(self::$iv_length);
+			$decryption_iv = random_bytes(self::$iv_length);
+			$decryption_key = openssl_digest(php_uname(), 'MD5', TRUE);
+			return openssl_decrypt($input, self::$ciphering, $decryption_key, self::$options, $encryption_iv);
+
+			//return trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $skey, self::url_base64_decode($input), MCRYPT_MODE_ECB, mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB), MCRYPT_RAND)));
 		}
 
 		public static function  url_base64_encode($str)
