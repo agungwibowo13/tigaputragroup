83317d8fdfc6f7d835808534c0f8d2c521d59de1
donniwibowo <donniwibowo15@gmail.com>
2022-02-13 08:23:45 +0700

    Set timezone to jakarta

--- pos/index.php
+++ pos/index.php
@@ -1,6 +1,7 @@
 <?php
 	ob_start();
 	session_start();
+	date_default_timezone_set("Asia/Jakarta");
 	
 	$module = isset($_GET['module']) ? strtolower($_GET['module']) : 'default';
 	$action = isset($_GET['action']) ? strtolower($_GET['action']) : 'cashier';
